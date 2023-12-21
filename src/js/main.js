62395758eecd7732b187c5f3c696ee5f21af9e42
Alesia Paulava <sportgirl16031988@gmail.com>
2023-12-21 16:58:06 +0300

    add media section promo features

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
+  const swiper = new Swiper('.swiper', {
+    loop: true, //бесконечная прокрутка слайдов
+    centeredSlides: true, //слайд центрируется в контейнере
+    spaceBetween: 30, //расстояние между слайдами 30px
+    slidesPerView: 1.4, //видимость слайда 1шт и еще чуть-чуть
+    initialSlide: 1, //начало с 1 слайда
+    autoplay: {
+      delay: 1500, //слайды листаются с задержкой в 1,5 сек
+      disableOnInteraction: false //если вручную переключать слайдер, autoplay не остановится
+    }
+  });
 })
 
